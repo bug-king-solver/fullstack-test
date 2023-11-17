88f18405408c7a30e32ba5d87ca7e932b725c048
George Rotaru <george.rotaru.pro@gmail.com>
2023-11-17 17:33:26 +0100

    feat: add SelectTime element in AppBar componenet

--- src/components/app-bar/AppBar.tsx
+++ src/components/app-bar/AppBar.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
-import MuiAppBar from '@mui/material/AppBar';
-import { styled } from '@mui/material/styles';
-import Typography from '@mui/material/Typography';
+import React, { useState } from "react";
+import MuiAppBar from "@mui/material/AppBar";
+import { styled } from "@mui/material/styles";
+import Typography from "@mui/material/Typography";
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+} from "@mui/material";
 
 const StyledAppBar = styled(MuiAppBar)`
   padding: 12px 30px;
@@ -13,6 +20,11 @@ export type AppBarProps = {
 };
 
 export const AppBar = ({ title }: AppBarProps) => {
+  const [timeLine, setTimeLine] = useState("");
+
+  const handleChangeTimeLine = (event: SelectChangeEvent) => {
+    setTimeLine(event.target.value as string);
+  };
   return (
     <StyledAppBar>
       <Typography
@@ -20,10 +32,26 @@ export const AppBar = ({ title }: AppBarProps) => {
         variant="h6"
         color="inherit"
         noWrap
-        sx={{ flexGrow: 1, alignSelf: 'center' }}
+        sx={{ flexGrow: 1, alignSelf: "center" }}
       >
-        { title }
+        {title}
       </Typography>
+      <FormControl variant="outlined" sx={{ minWidth: 160, marginLeft: 2 }}>
+        <InputLabel id="time-line-select-label">Select Time Line</InputLabel>
+        <Select
+          value={timeLine}
+          onChange={handleChangeTimeLine}
+          sx={{ color: "inherit" }}
+          label="Select Time Line"
+          labelId="time-line-select-label"
+          id="time-line-select"
+        >
+          <MenuItem value="today">Today</MenuItem>
+          <MenuItem value="yesterday">Yesterday</MenuItem>
+          <MenuItem value="lastSevenDays"> Last seven days</MenuItem>
+          <MenuItem value="thisMonth">This month</MenuItem>
+        </Select>
+      </FormControl>
     </StyledAppBar>
   );
 };
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,4 @@
+import { Container, Paper } from "@mui/material";
 import useGetAllArticles from "../api/hooks/useGetAllArticles";
 import { Article } from "../components";
 
@@ -6,17 +7,19 @@ const HomePage = () => {
   if (isLoading) return <div>IsLoading</div>;
   if (error) return <div>Error</div>;
   return (
-    <div>
-      {articles?.map((article) => (
-        <Article
-          key={article.id}
-          author={article.author}
-          url={article.url}
-          image={article.image_url}
-          traffic={article.daily_traffic}
-        />
-      ))}
-    </div>
+    <Container maxWidth="lg">
+      <Paper>
+        {articles?.map((article) => (
+          <Article
+            key={article.id}
+            author={article.author}
+            url={article.url}
+            image={article.image_url}
+            traffic={article.daily_traffic}
+          />
+        ))}
+      </Paper>
+    </Container>
   );
 };
 
